505e60aa025b899ae8c406d556eb1d083a308e20
Sagar Barate <sagarbarate2907@com.com>
2024-11-05 00:24:11 +0000

    intial commit

---
+++ carRentalSystem/src/carRentalSystem/Car.java
@@ -0,0 +1,18 @@
+package carRentalSystem;
+
+public class Car {
+	private int ID;
+	private String brand;
+	private String model;
+	private String color;
+	private String color;
+	private int year;
+	private double price;
+	private boolean available;
+	
+	public Car() {}
+	
+	public int getID() {
+		return ID;
+	}
+}
